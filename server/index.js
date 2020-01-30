e3eff9e3cde5c5b483f4df999cd11865f719d3ba
Ryan Powell <ryan.powell.hkrctr@gmail.com>
2020-01-29 20:21:49 -0800

    OMG Promises Work!

--- server/index.js
+++ server/index.js
@@ -1,8 +1,5 @@
-const express = require('express');
+const app = require('./app');
 
-const app = express();
 const port = 4444;
-
-app.use(express.static('./client/dist'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
